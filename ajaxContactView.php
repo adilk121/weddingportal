0ef8366b2171bbf3cd846efeabf8e12fa8e10174
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-03-28 17:23:56 +0530

    Update ajaxContactView.php

--- ajaxContactView.php
+++ ajaxContactView.php
@@ -2,7 +2,6 @@
 require_once("includes/dbsmain.inc.php");
 date_default_timezone_set("asia/kolkata");
 $date_time=date("Y-m-d H:i:s");
-//header time and date
 db_query("insert into tbl_contact_view set
 cv_view_by_mem_id='$_REQUEST[view_by_memid]',
 cv_view_to_mem_id='$_REQUEST[view_to_memid]',
