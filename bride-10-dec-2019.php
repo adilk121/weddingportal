37f6be7d71cbe5710f77a9dc386ecfbacffb6052
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-04-30 12:24:09 +0530

    Update bride-10-dec-2019.php

--- bride-10-dec-2019.php
+++ bride-10-dec-2019.php
@@ -3,7 +3,7 @@
 
     <link rel="stylesheet" href="assets/owlcarousel/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/owlcarousel/assets/owl.theme.default.min.css">
-<?php //header?>
+
     
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
     <link rel="shortcut icon" href="assets/ico/favicon.png">
