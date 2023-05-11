8e787edf1d5240950f16c651d87efb4acdbed77b
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-05-12 00:33:30 +0530

    Update check-member-email.php

--- check-member-email.php
+++ check-member-email.php
@@ -1,6 +1,5 @@
 <?php 
 require_once("includes/dbsmain.inc.php");
-//header
 $sql="SELECT reg_member_verified_email FROM  tbl_registration WHERE reg_id='$_SESSION[regID]'";
 $MemMobile=db_scalar($sql);
 if(!empty($MemMobile)){
