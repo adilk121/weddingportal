f57bc3835e606da404d9f66fa17ed9490e56d1e1
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-05-13 02:07:25 +0530

    Update add-service-cat.php

--- add-service-cat.php
+++ add-service-cat.php
@@ -3,6 +3,7 @@ $db = mysqli_connect('localhost','thediann_bestweddinghub','!+.ub,fL$O7Y','thedi
 
 if(isset($_POST['submit']))
 {
+  //header
 $name = $_POST['name'];
 
 $sql= "INSERT INTO `service_cat`(`name`) VALUES ('$name')";
