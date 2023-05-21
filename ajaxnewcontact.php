145a3f090638e15169e0ebce9f8898c858d3a37b
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-05-21 10:46:00 +0530

    Update ajaxnewcontact.php

--- ajaxnewcontact.php
+++ ajaxnewcontact.php
@@ -3,6 +3,7 @@ require_once("includes/dbsmain.inc.php");
 date_default_timezone_set("asia/kolkata");
 $date_time=date("Y-m-d H:i:s");
 
+//header
   //header
 db_query("insert into tbl_contact_view set
 cv_view_by_mem_id='$_REQUEST[view_by_memid]',
