9ff06b6de90f17f3628edbaaed3af20d9557102f
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-05-30 17:14:01 +0530

    Update bride.php

--- bride.php
+++ bride.php
@@ -1,6 +1,7 @@
 <meta name="robots" content="index, follow" />
     <link rel="stylesheet" href="assets/css/docs.theme.min.css">
 <?php 
+//header
  ?>
  <link rel="stylesheet" href="assets/owlcarousel/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/owlcarousel/assets/owl.theme.default.min.css">
