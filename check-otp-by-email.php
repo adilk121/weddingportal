a14fecdfec8d5b40a94d4ee0c6c752e301337b16
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-06-03 15:04:15 +0530

    Update check-otp-by-email.php

--- check-otp-by-email.php
+++ check-otp-by-email.php
@@ -5,6 +5,7 @@ $otp=$_GET['otp'];
 $regID=$_GET['regID'];
 $regID=$_GET['regID'];
 
+  //header
 if($otp!="" && $regID!=""){
 	
 $rID=db_scalar("SELECT reg_id FROM tbl_registration WHERE 1 AND reg_id='$regID'");	
