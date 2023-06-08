e8f1b6082fdc5edb98e10b7d5de26763d807cd9d
ADY <112151943+ady1210@users.noreply.github.com>
2023-06-08 13:50:57 +0530

    Update check-member-photo.php

--- check-member-photo.php
+++ check-member-photo.php
@@ -3,7 +3,6 @@ require_once("includes/dbsmain.inc.php");
 $sql="SELECT reg_profile_photo FROM  tbl_registration WHERE reg_id='$_SESSION[regID]'";
 $MemPhoto=db_scalar($sql);
 if(!empty($MemPhoto)){
-  //header
 echo "1";	
 }else{
 echo "0";		
