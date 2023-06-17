15a2dedc6d25db770ffca0ff14e8b19c2a54ad0a
ADY <112151943+ady1210@users.noreply.github.com>
2023-06-17 12:49:37 +0530

    Update check-member-mobile.php

--- check-member-mobile.php
+++ check-member-mobile.php
@@ -3,7 +3,7 @@ require_once("includes/dbsmain.inc.php");
 $sql="SELECT reg_member_verified_mobile FROM  tbl_registration WHERE reg_id='$_SESSION[regID]'";
 $MemMobile=db_scalar($sql);
 if(!empty($MemMobile)){
-  
+  //h
 echo "1";	
 }else{
 echo "0";		
