73ddfa68cc12983d0dd472f544a9781154fc55b9
Adil Khan <112151943+ady1210@users.noreply.github.com>
2023-06-21 16:13:09 +0530

    Update check-otp-by-email.php

--- check-otp-by-email.php
+++ check-otp-by-email.php
@@ -1,5 +1,6 @@
 <?php
 require_once("includes/dbsmain.inc.php");
+//header
 ////////////////////////   FOR MEMBER MOBILE VERIFICATION /////////////////////////////
 $otp=$_GET['otp'];
 $regID=$_GET['regID'];
