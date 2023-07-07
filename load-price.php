828571ed74658db9b6f6d741e8b4e09b5b514fd8
Adil Khan <112151943+ady1210@users.noreply.github.com>
2023-07-07 14:41:25 +0530

    Update load-price.php

--- load-price.php
+++ load-price.php
@@ -2,6 +2,7 @@
 ob_start();
 require_once("includes/dbsmain.inc.php"); 
 include("site-main-query.php");
+  //header
 
 $sql="SELECT md_price FROM tbl_membership_detail WHERE md_id=$mdID";
 $mPrice=db_scalar($sql);
