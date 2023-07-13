54036507fbff04839dafbca02e6f3689bc1c9367
Adil Khan <112151943+ady1210@users.noreply.github.com>
2023-07-13 18:39:51 +0530

    Update geo_location.php

--- geo_location.php
+++ geo_location.php
@@ -1,7 +1,6 @@
 <?php
 require_once('geoplugin.class.php');
 $geoplugin = new geoPlugin();
-// If we wanted to change the base currency, we would uncomment the following line
 // $geoplugin->currency = 'EUR';
  
 $geoplugin->locate();
